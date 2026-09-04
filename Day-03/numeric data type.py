4ae6a3d810391748167d2db02d42917d7297815a
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 00:33:03 +0530

    numeric data type

--- Day-03/numeric data type.py
+++ Day-03/numeric data type.py
@@ -2,6 +2,7 @@
 number = 10
 cgpa = 8.5
 c=2+8j
+#j is the imaginary unit,2 is the real part and 8 is the imaginary part of complex number.
 print(type(number))
 print(type(cgpa))   
 print(type(c))
