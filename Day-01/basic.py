a39ee02baeebd717f217214f40c9463dc267d508
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-04 23:39:49 +0530

    finding aera ,perimeter

--- Day-01/basic.py
+++
@@ -1,6 +0,0 @@
-length =int(input("enter length value:"))
-breadth =int(input('enter breadth value:'))
-aera = length*breadth
-perimeter =2*(length+breadth)
-print("the aera of rectangle:"+ str(aera))
-print("the perimeter of rectangle:"+ str(perimeter))
