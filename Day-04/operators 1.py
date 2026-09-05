63ae862e746c021b91aa7a76d90db53e1c7603a6
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 12:15:17 +0530

    arthimetic operators

---
+++ Day-04/operators 1.py
@@ -0,0 +1,12 @@
+#arthimetic operators
+a=10
+b=5
+print(a+b,a-b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+#assiginment operators
+print(5<<2)#left shift.
+print(5>>2)#right shift.
