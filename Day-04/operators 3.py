4441de556bbe376a4aa082014030d20c175fd8f7
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 12:27:31 +0530

    #Comparision Operators.

---
+++ Day-04/operators 3.py
@@ -0,0 +1,9 @@
+#Comparision Operator
+a=26
+b=45
+print("a is not greater than b:",a>b)
+print("b is greater than b:",a<b)
+print(a>=b)#a is greater than or equal to  b:
+print(a<=b)#a is less than or equal to b:
+print(a==b)
+print(a!=b)#a value is not equal to b:
