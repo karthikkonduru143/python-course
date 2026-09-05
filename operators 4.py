dcd554d8063166fb8e494fa0d2c9fe17ccf0cd40
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 12:40:40 +0530

    Logical Operators.

---
+++ operators 4.py
@@ -0,0 +1,13 @@
+#Logical Operators
+a=26
+b=45
+print(a>b and a<b) #evry condition will be true:then it can be print true other wise false.
+print(a<=b and a!=b)# here both conditions are true. then out put is true.
+print(a==b and b>=a)
+print()
+print(a>b or a<b)#if one condition true : then output is true.
+print(a<=b or a!=b)
+print(a==b or a>b )#both conditions are false then output is false.
+print()
+print(not a>b)#the condition should be reverse.if orginal condition true if we place not then value is reverse.
+print(not a<b)
