63053c659e758e67ce04e534cf70af8e98af6433
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 11:13:46 +0530

    sets & dictionary

---
+++ Day-03/mapping datatype.py
@@ -0,0 +1,11 @@
+#sets & dictionaries
+a=set()#It can print the empty set.
+d={}
+set={1,2,3,4,5}
+d={'name':'karthik','age':22,'gender':'male'}
+print(d.values())#it can print values of a dictionary.
+print(d.keys())#it can print keys of a dictionary.
+print(d.items())# it can print keys and values of a dictionary.
+print("set is printed:",set)
+print(type(a))
+print(type(d))
