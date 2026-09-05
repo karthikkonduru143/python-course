1967dfebde6d689de84c87ead1607962dbaadde0
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-05 16:22:51 +0530

    Identity operator.

---
+++ Day-04/operators 5.py
@@ -0,0 +1,7 @@
+#Identity operator
+l1=[10,20,30]
+l2=[10,20,30]
+l3=l1
+print(l1 is l2)#it can print false why because l1 object and l2 object is different.(object means data stored place)
+print(l1 is l3)#if we take new variable l3 and then l3 is equal l1. then it can print true.
+print(l1==l3)
