5bfb321c123e6868f949abeeae707f92c42505df
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-06 15:10:36 +0530

    string slicing.

---
+++ Day-05/string2.py
@@ -0,0 +1,6 @@
+s='Python Programming'
+print(s[3:10:])#satarting index "3",ending is index"10".
+print(s[3:10:2])##satarting index "3",ending is index"10" ang skip the value and print next index.
+print(s[:5])#ending the string at 5th index.
+print(s[6:])#starting index aat 6th.
+print(s[::-1])#it an print reverse
