58b563737b43626c5ec278a57bec24d2f3d5e493
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-06 15:21:00 +0530

    String Operations.

---
+++ string3.py
@@ -0,0 +1,7 @@
+#String Operations
+s1='codegnan'
+s2='students'
+print(s1+s2)# it can add the two dieffernt strings.
+print(s1*4)# it can print a sting in multple times.
+print(s2*3)#it can print a sting in multple times.
+print(s1<s2)
